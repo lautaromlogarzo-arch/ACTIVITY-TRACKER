abf32dbefa45fd66abc0b15787360fa48098ad12
LautaroL <lautaromlogarzo@gmail.com>
2026-01-09 12:25:16 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+// CAMBIÉ "v1" POR "v2" AQUÍ ABAJO PARA FORZAR LA ACTUALIZACIÓN
+const CACHE_NAME = "fsae-tracker-v2"; 
+
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./logo.png",
+  "https://cdn.jsdelivr.net/npm/chart.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+];
+
+// Instalar y guardar archivos
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activar y limpiar versiones viejas (ESTO BORRARÁ LA v1 Y PONDRÁ LA v2)
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Servir archivos
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
